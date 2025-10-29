c246bbf5f8f57e7354c2d61684bb789fcbd0d65d
nicholasngje-jpg <nicholas.ngje@gmail.com>
2025-10-30 01:17:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Required for flashing messages
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def index():
